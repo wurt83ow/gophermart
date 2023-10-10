07342cb98afed9ede653dcbc50514cdc6dcc0b58
wurt83ow <wurt83ow@yandex.ru>
2023-10-10 17:08:06 +0300

    optimization of structures from the models3

--- internal/models/models.go
+++ internal/models/models.go
@@ -42,14 +42,6 @@ type ExtRespOrder struct {
 	Accrual float32 `db:"accrual" json:"accrual,omitempty"`
 }
 
-// type BDOrder struct {
-// 	Order string `db:"order" `
-// }
-
-// type BDAccrual struct {
-// 	Accrual float32 `db:"accrual" json:"accrual,omitempty"`
-// }
-
 type DataWithdraw struct {
 	UserID  string    `db:"user_id" json:"user_id"`
 	Order   string    `db:"order" json:"order"`
