03dc8980646bd663a8849bc7ee22fe1f2be46c6a
wurt83ow <wurt83ow@yandex.ru>
2023-10-27 18:10:03 +0300

    bdKeeper.go error correction1

--- internal/bdkeeper/bdKeeper.go
+++ internal/bdkeeper/bdKeeper.go
@@ -13,12 +13,12 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	// _ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/google/uuid"
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
 
-	// _ "github.com/jackc/pgx/v5/stdlib"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/wurt83ow/gophermart/internal/models"
 	"github.com/wurt83ow/gophermart/internal/storage"
 	"go.uber.org/zap"
