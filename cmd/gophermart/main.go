f32a6d23f693fa41af7139b6432de147f9132851
wurt83ow <wurt83ow@yandex.ru>
2023-10-28 17:45:28 +0300

    create new branch part3

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/wurt83ow/gophermart/internal/app"
+import (
+	"log"
+	"os"
+	"runtime/pprof"
+
+	"github.com/wurt83ow/gophermart/internal/app"
+)
 
 func main() {
+	fl, err := os.Create("./cpu.pprof")
+	if err != nil {
+		log.Fatal()
+	}
+	defer fl.Close()
+
+	pprof.StartCPUProfile(fl)
+	defer pprof.StopCPUProfile()
+
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
